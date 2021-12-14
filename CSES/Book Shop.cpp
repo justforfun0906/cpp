1c7cca688a4729e50501690426e15a804a85ea71
justforfun0906 <87445512+justforfun0906@users.noreply.github.com>
2021-12-14 16:32:19 +0800

    Create Book Shop.cpp

---
+++ CSES/Book Shop.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1005][1005];
+int page[1005];
+int price[1005];
+int main(){
+	int n,x;
+	cin>>n>>x;
+	for(int i=0;i<n;i++) cin>>price[i];
+	for(int i=0;i<n;i++) cin>>page[i]; 
+	for(int i=0;i<n;i++){
+		for(int j=0;j<x;j++){
+			
+		}
+	}
+}
+
