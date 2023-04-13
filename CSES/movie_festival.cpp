7734d20fc18686a07a4292476c33bd19a8516292
justforfun0906 <jimmy0906abc@gmail.com>
2023-04-13 22:55:52 +0800

    finished movie festival

--- CSES/movie_festival.cpp
+++ CSES/movie_festival.cpp
@@ -1,4 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n;
+    cin>>n;
+    vector<pair<int, int> > arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i].second>>arr[i].first;
+    }
+    sort(arr.begin(),arr.end());
+    int cnt=0;
+    int cur_end=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i].second>=cur_end){
+            cur_end=arr[i].first;
+            cnt++;
+        }
+    }
+    cout<<cnt;
 }
