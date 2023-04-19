32aec194ae84f7357bacb45182d7c4ec8fbf8354
justforfun0906 <jimmy0906abc@gmail.com>
2023-04-19 20:37:19 +0800

    grinding 135

--- sprout/135.cpp
+++ sprout/135.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+long long ans;
 void merge_sort(vector<int> a, int l, int r){//[]
     if(l==r)return;
     int mid = (l+r)/2;
@@ -8,10 +9,14 @@ void merge_sort(vector<int> a, int l, int r){//[]
     vector<int> sorted(r-l+1);
     int li=l,ri=mid+1; //left iterator, right iterator
     int iter=0;//iterator of sorted array
+    
     for(;li<=mid;li++){
         while(ri<=r&&a[ri]<a[li]){
             sorted[iter]=a[ri];
             ri++;iter++;
+            ans+=a[ri]+a[li];
+            //cout<<a[ri]<<' '<<a[li]<<'\n';
+            ans=ans%(int)1e7+19;
         }
         sorted[iter]=a[li];
         iter++;
@@ -20,19 +25,17 @@ void merge_sort(vector<int> a, int l, int r){//[]
         sorted[iter]=a[ri];
         ri++;iter++;
     }
-    for(int i=l;i<=r;i++){
-        a[i]=sorted[i-l];
+    for(int i=0;i<r-l;i++){
+        a[l+i]=sorted[i];
     }
-    cout<<"l="<<l<<" r= "<<r<<'\n';
-    for(int i=0;i<=r;i++){
-        cout<<sorted[i]<<' ';
-    }
-    cout<<'\n';
 }
 int main(){
     int n;
     cin>>n;
     vector<int> arr(n);
     for(int i=0;i<n;i++)cin>>arr[i];
+    ans=0;
     merge_sort(arr,0,n-1);
+    for(int i=0;i<n;i++)cout<<arr[i]<<' ';
+    //cout<<ans/2;
 }
