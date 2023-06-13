c742dced565784679dd71e4a530f3586e99e6b2b
justforfun0906 <jimmy0906abc@gmail.com>
2023-06-13 19:46:49 +0800

    a

--- CSES/Stick_Lengths.cpp
+++ CSES/Stick_Lengths.cpp
@@ -8,7 +8,9 @@ int main(){
         cin>>a[i];
     }
     sort(a.begin(),a.end());
+    for(int i=0;i<n;i++)cout<<a[i];
     int t=a[n/2];
+    cout<<t;
     long long sum=0;
     for(int i=0;i<n;i++){
         sum+=abs(t-a[i]);
