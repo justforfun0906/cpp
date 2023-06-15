23816bff848ea462491ab68585c5c12dee0a82f3
justforfun0906 <justforfun0906abc@gmail.com>
2023-06-15 23:45:18 +0800

    making h663

---
+++ C++/h663.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int N;
+    cin>>N;
+    int g[N+5][N+5];
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=N;j++){
+            cin>>g[i][j];
+        }
+    }
+}
