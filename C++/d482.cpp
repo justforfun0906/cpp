a4b856970aeb9278abada05190bcc14e0b159c73
justforfun0906 <jimmy0906abc@gmail.com>
2023-06-19 15:41:25 +0800

    d482 complete

--- C++/d482.cpp
+++ C++/d482.cpp
@@ -4,17 +4,25 @@ long long dp[105][105]={0};
 int a[105][105]={0};
 int main(){
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+    while(cin>>n){
+        for(int i=0;i<105;i++){
+            for(int j=0;j<105;j++){
+                dp[i][j]=0;
+                a[i][j]=0;
+            }
+        }
+        for(int i=1;i<=n;i++){
+          for(int j=1;j<=n;j++){
             cin>>a[i][j];
+            }
         }
-    }
-    dp[1][1]=a[1][1];
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            dp[i][j]=a[i][j]+max(dp[i-1][j],dp[i][j-1]);
+        dp[1][1]=a[1][1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]=a[i][j]+max(dp[i-1][j],dp[i][j-1]);
+            }
         }
+        cout<<dp[n][n]<<'\n';
     }
-    cout<<dp[n][n]<<'\n';
+    
 }
