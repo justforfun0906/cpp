7c1ab5ac1e12b631cd6ee80b984d665aabcc0f97
justforfun0906 <justforfun0906abc@gmail.com>
2023-06-23 19:23:56 +0800

    don't know what happened

--- Codeforces/#881 div3/pE.cpp
+++ Codeforces/#881 div3/pE.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 vector<int> change(200005,0);
 vector< vector<int> > prefix_loc(100005);
-bool check(int c){
-
+bool check(int l,int r,int c,int n){
+    vector<int> prefix(n+5,0),cnt(n+5,0);
+    for(auto x:prefix_loc[c]){
+        prefix[x]=1;
+    }
+    for(int i=1;i<=n;i++){
+        cnt[i]=prefix[i]+cnt[i-1];
+    }
+    int one_cnt=cnt[r]-cnt[l-1];
+    int zero_cnt=r-l+1-one_cnt;
+    if(one_cnt>zero_cnt)return 1;
+    else return 0;
 }
 void solve(){
     int n,m;
@@ -21,11 +31,18 @@ void solve(){
     int l=0,r=q-1;
     while(l<r){
         int mid= (l+r)/2;
-        if(check(mid)){
+        bool ok=0;
+        for(int i=0;i<m;i++){
+            if(check(segs[i].first,segs[i].second,mid,n)){
+                ok=1;
+            }
+        }
+        if(ok){
             r=mid;
         }
         else l=mid+1;
     }
+    cout<<l+1<<'\n';
 }
 int main(){
     int t;cin>>t;
