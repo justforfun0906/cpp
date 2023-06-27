9b2036c5dcd4bb11b1d3ae0e713d85ca63dbd08f
justforfun0906 <jimmy0906abc@gmail.com>
2023-06-27 17:24:58 +0800

    tracking segments complete!

--- Codeforces/#881 div3/pE.cpp
+++ Codeforces/#881 div3/pE.cpp
@@ -1,81 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> change(100005);
-
-bool check(vector<int> cnt,int l,int r){
-    int one_cnt=cnt[r]-cnt[l-1];
-    int zero_cnt=r-l+1-one_cnt;
-    if(one_cnt>zero_cnt)return 1;
-    else return 0;
+int n,m;
+bool check(vector<pair<int,int>> segments, vector<int> change, int mid){
+    vector<int> prefix(n+5,0), prefix_sum(n+5,0);
+    for(int i=1;i<=mid;i++)prefix[change[i]]=1;
+    for(int i=1;i<=n;i++)prefix_sum[i]=prefix_sum[i-1]+prefix[i];
+    for(int i=0;i<m;i++){
+        int one=prefix_sum[segments[i].second]-prefix_sum[segments[i].first-1];
+        int zero=segments[i].second-segments[i].first+1-one;
+        if(one>zero)return true;
+    }
+    return false;
 }
-
 void solve(){
-    for(int i=0;i<100005;i++)change[i]=0;
-    int n,m;
     cin>>n>>m;
-    vector<pair<int,int>> segs(m);
-    for(int i=0;i<m;i++){
-        cin>>segs[i].first>>segs[i].second;
-    }
+    vector< pair<int, int> > segments(m+5);
+    for(int i=0;i<m;i++) cin>>segments[i].first>>segments[i].second;
     int q;cin>>q;
-    for(int i=0;i<q;i++){
-        cin>>change[i];
+    vector<int> change(q+5);
+    for(int i=1;i<=q;i++) cin>>change[i];
+    int l=1, r=q;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(check(segments,change,mid))r=mid-1;
+        else l=mid+1;
+        
     }
-    int l=0,r=q-1;
-    bool stop=0;
-    bool ok_temp=0;
+    if(l>q)cout<<-1<<'\n';
+    else cout<<l<<'\n';
 
-    vector<int> prefix(n+5,0),cnt(n+5,0);
-    for(int i=0;i<=q;i++){
-        prefix[change[i]]=1;
-    }
-    for(int i=1;i<=n;i++){
-        cnt[i]=prefix[i]+cnt[i-1];
-    }
-    for(int i=0;i<m;i++){
-        if(check(cnt,segs[i].first,segs[i].second)){
-           ok_temp=1; 
-        }
-    }
-    if(ok_temp==0){
-        stop=1;
-        cout<<-1<<'\n';
-    }
-
-    if(stop==0){
-        while(l<r){
-            int mid= (l+r)/2;
-            bool ok=0;
-            for(int i=0;i<n+5;i++){
-                prefix[i]=0;
-                cnt[i]=0;
-            }
-            for(int i=0;i<=mid;i++){
-                prefix[change[i]]=1;
-            }
-            for(int i=1;i<=n;i++){
-                cnt[i]=prefix[i]+cnt[i-1];
-            }
-            for(int i=0;i<m;i++){
-                if(check(cnt,segs[i].first,segs[i].second)){
-                    ok=1;
-                }
-            }
-            if(ok){
-                r=mid;
-            }
-            else l=mid+1;
-        }
-        cout<<l+1<<'\n';
-    }
 }
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int t;cin>>t;
-    while(t--){
-        solve();
-    }
+    while(t--)solve();
 }
