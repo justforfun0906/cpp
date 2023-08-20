88144fffb126574da9fb498c1bb92b8ed559db49
justforfun0906 <jimmy0906abc@gmail.com>
2023-08-21 00:42:30 +0800

    pE AC

--- atcoder/abc315/pE.cpp
+++ atcoder/abc315/pE.cpp
@@ -2,9 +2,10 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+vector<vector<int> > g(200005);
 vector<bool> check(200005, 0);
 vector<int> ans;
-void dfs(int n,vector<vector<int> >g, int node){
+void dfs(int n, int node){
 	if(g[node].empty()){// leaf node
 		if(check[node]==0){
 			ans.push_back(node);
@@ -14,7 +15,7 @@ void dfs(int n,vector<vector<int> >g, int node){
 	}
 	else{
 		for(int i=0;i<g[node].size();i++){
-			if(check[g[node][i]]==0)dfs(n, g, g[node][i]);
+			if(check[g[node][i]]==0)dfs(n, g[node][i]);
 		}
 		check[node]=1;
 		ans.push_back(node);
@@ -24,7 +25,6 @@ void dfs(int n,vector<vector<int> >g, int node){
 int main(){
 	int n; 
 	cin>>n;
-	vector<vector<int> > g(n+10);
 	for(int i=0;i<n;i++){
 		int m; cin>>m;
 		for(int j=0;j<m;j++){
@@ -32,6 +32,6 @@ int main(){
 			g[i+1].push_back(temp);
 		}
 	}
-	dfs(n, g, 1);
+	dfs(n, 1);
 	for(auto x:ans)if(x!=1)cout<<x<<' ';
 }
