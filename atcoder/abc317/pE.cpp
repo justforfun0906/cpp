82487f1f59f7c53f321792b2c0f23bf803f71ccf
justforfun0906 <jimmy0906abc@gmail.com>
2023-08-31 20:31:13 +0800

    macbook3

---
+++ atcoder/abc317/pE.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+using namespace std;
+int main(){
+	int n,m; cin>>n>>m;
+	int arr[n+5][m+5];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			char temp; cin>>char;
+		}
+	}
+}
