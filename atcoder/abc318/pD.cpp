016616c7717c424ba063c954a6a6bdbadca85dae
justforfun0906 <jimmy0906abc@gmail.com>
2023-09-02 21:49:22 +0800

    318

---
+++ atcoder/abc318/pD.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    long long n, ans=0; cin>>n;
+    vector<vector<long long> > find(n+5);
+    for(long long i=0;i<n;i++){
+        long long temp; cin>>temp;
+        find[temp].push_back(i);
+    }
+    for(long long i=1;i<=n;i++){
+        if(find[i].size()>1){
+            long long end = find[i].size();
+            long long acc=0;
+            for(long long j=0;j<end;j++){
+                ans +=j*find[i][j]-acc;
+                acc+=find[i][j]+1;
+                //cout<<"i="<<i<<" acc="<<acc<<'\n';
+            }
+            ans-= ((end-2)*(end-1))/2;
+        }
+        cout<<ans<<'\n';
+    }
+    cout<<ans;
+}
