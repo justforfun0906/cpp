0bc05cb59cb7b33e5667b7ab13ffd98b37512fac
justforfun0906 <jimmy0906abc@gmail.com>
2023-09-21 15:10:00 +0800

    bruh

---
+++ nthu_oj/nthu cp/pB.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    int w_cnt = 0, b_cnt = 0;
+    cin>>n>>m;
+    char map[n+5][m+5];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>map[i][j];
+        }
+    }
+}
