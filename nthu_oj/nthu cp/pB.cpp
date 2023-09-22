e98330703d56b61d10ecab78d03ba9d633316e56
justforfun0906 <jimmy0906abc@gmail.com>
2023-09-22 10:09:17 +0800

    what

--- nthu_oj/nthu cp/pB.cpp
+++ nthu_oj/nthu cp/pB.cpp
@@ -10,4 +10,28 @@ int main(){
             cin>>map[i][j];
         }
     }
+    int status=0; //1 = white, -1 = black, 0 = *
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(map[i][j]=='W'){
+                if(status!=1){
+                    w_cnt++;
+                    status = 1;
+                }
+            }else if(map[i][j]=='B'){
+                
+            }else if(map[i][j]=='G'){
+                if(map[i][j-1]!='G'){
+                    if(status==1){
+                        b_cnt++;
+                    }else{
+                        
+                    }
+                }
+
+            }else{
+
+            }
+        }
+    }
 }
