4b74008bdcc6707843e6d537d1db18273d29da1a
justforfun0906 <jimmy0906abc@gmail.com>
2023-10-07 23:15:02 +0800

    g

---
+++ nthu_oj/14020.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int solve(char s[10005]){
+	int ans=0;
+	int len = strlen(s);
+	return len;
+}
+int main(){
+	char s[10005];
+	while(scanf("%s", s)){
+		int ans = solve(s);
+	}
+}
