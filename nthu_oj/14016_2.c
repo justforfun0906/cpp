3c2bdc997867431aab7a0a06bf8ac7ab6147cd85
justforfun0906 <jimmy0906abc@gmail.com>
2023-10-10 15:55:52 +0800

    hu who?

--- nthu_oj/14016_2.c
+++ nthu_oj/14016_2.c
@@ -22,14 +22,14 @@ int main(){
     //make use of the value of i-j to determine the iter for dl and dr
     for(int i=0;i<N;i++){
         long long temp=0;
-        for(int j=N-1-i, k=0;j<N, k<N; j++, k++){
+        for(int j=N-1-i, k=0;j<N&&k<N; j++, k++){
             temp += m[j][k];
         }
         dr[i]= temp;
     }
     for(int i=1;i<N;i++){
         long long temp = 0;
-        for(int j=i, k=0;j<N, k<N; j++, k++){
+        for(int j=i, k=0;j<N&&k<N; j++, k++){
             temp += m[k][j];
         }
         dr[N-1+i]=temp;
@@ -37,16 +37,34 @@ int main(){
     //fuck you
     for(int i=0;i<N;i++){
         long long temp = 0;
-        for(int j=i, k=0; j<N, k>=0, j++,k--){
-            temp += m[j][k];
+        for(int j=i, k=0; j>=0&&k<N; j--,k++){
+            temp += m[k][j];
         }
         dl[i]=temp;
     }
     for(int i=1;i<N;i++){
         long long temp = 0;
-        for(int j=i, k = N-1; j<N, k>=0; j++, k--){
+        for(int j=i, k = N-1; j<N&&k>=0; j++, k--){
             temp += m[j][k];
         }
         dl[N-1+i]=temp;
+    }
+    /*for(int i=0;i<=2*N-2;i++){
+        printf("%lld ", dr[i]);
+    }
+    printf("\n");
+    for(int i=0;i<=2*N-2;i++){
+        printf("%lld ", dl[i]);
+    }*/
+    while(t--){
+        int x,y;
+        scanf("%d %d", &x, &y);
+        long long ans =0;
+        ans += h[x];
+        ans += v[y];
+        if(x>=y) ans +=dr[(N-1)-(x-y)];
+        else ans += dr[(N-1)+(y-x)];
+        ans += dl[x+y];
+        printf("%lld\n", ans-3*m[x][y]);
     }
 }
