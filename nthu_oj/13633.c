c6f623954698fb4519e8761b5274aeb0997de90f
justforfun0906 <jimmy0906abc@gmail.com>
2023-10-14 20:43:20 +0800

    all perfecto

--- nthu_oj/13633.c
+++ nthu_oj/13633.c
@@ -3,23 +3,20 @@
 #include<stdbool.h>
 int main(){
     char s[100000];
-    while(scanf("%[^\n]", s)!=EOF){
-        char sub[10000];
-        for(int i=0;i<8;i++)sub[i]=s[i];
-        if(strcmp(sub,"#include")==0){
-            printf("%s\n",s);
-        }else{
-            int size = strlen(s);
-            bool c = s[size-1]=='{'||s[size-1]=='}';
-            for(int i=0;i<size;i++){
-                if(s[i]==';'&&i!=size-1){
-                    printf(' ');
-                }else{
-                    printf("%c", s[i]);
-                }
+    while(scanf("%[^\n]%*c", s)!=EOF){
+        bool middle=0;
+        int size = strlen(s);
+        bool c = s[size-1]=='{'||s[size-1]=='}'||s[size-1]==';'||s[size-1]=='>';
+        for(int i=0;i<size;i++){
+            if(s[i]==';'&&i!=size-1){
+                if(middle==0)printf("%c",' ');
+                else printf("%c",';');
+            }else{
+                printf("%c", s[i]);
+                if(s[i]!='}') middle=1;
             }
-            if(c==0)printf(";\n");
-            else printf("\n");
         }
+        if(c==0||(middle&&s[size-1]=='}'))printf(";\n");
+        else printf("\n");
     }
 }
