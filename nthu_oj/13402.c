36bbc8bb8c5daee3ac1843f1f0bc50b55c2d2637
justforfun0906 <jimmy0906abc@gmail.com>
2023-12-16 00:49:52 +0800

    bruh

--- nthu_oj/13402.c
+++ nthu_oj/13402.c
@@ -2,21 +2,28 @@
 int board[16][16];
 int check(int n, int m){
     int flag = 1;
+    //printf("now:\n");
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(i-1>=0&&board[i-1][j]!=board[i][j]) flag = 0;
-            if(j<m-1&&board[i][j]!=board[i][j+1]) flag = 0;
+            if(i-1>=0){
+                if(board[i][j]!=board[i-1][j]) flag = 0;
+            }
+            if(j<m-1){
+                if(board[i][j]!=board[i][j+1]) flag = 0;
+            }
             if(flag != 1)break;
+           // printf("%d",board[i][j]);
         }
+        //printf("\n");
     }
     return flag;
 }
 void mod(int n,int m,int i, int j){
     board[i][j]=(board[i][j]+1)%2;
-    if(i-1>=0)board[i-1][j]=(board[i-1][j]+1)%2;
-    if(i+1<n)board[i+1][j]=(board[i+1][j]+1)%2;
-    if(j-1>=0)board[i][j-1]=(board[i][j-1]+1)%2;
-    if(j+1<m)board[i][j+1]=(board[i][j+1]+1)%2;
+    if(i-1>=0) board[i-1][j]=(board[i-1][j]+1)%2;
+    if(i+1<n) board[i+1][j]=(board[i+1][j]+1)%2;
+    if(j-1>=0) board[i][j-1]=(board[i][j-1]+1)%2;
+    if(j+1<m) board[i][j+1]=(board[i][j+1]+1)%2;
 }
 int flip(int n,int m, int last_i, int last_j,int cnt){//
     int result = -1;
@@ -37,15 +44,15 @@ int flip(int n,int m, int last_i, int last_j,int cnt){//
     return result;
 }
 int main(){
-    int n, int m;
     int n, m;
-    scanf("%d %d",&n,&m);
+    scanf("%d %d\n",&n,&m);
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             char temp;
-            scanf("%c",temp);
-            if(temp=='b')board[i][j]=1;
-            else board[i][j]=0;
+            temp = getchar();
+            if(temp =='b')board[i][j] = 1;
+            else if(temp == 'w')board[i][j] = 0;
+            temp = getchar();
         }
     }
     int result = flip(n,m,-1,-1,0);
--- nthu_oj/test.c
+++ nthu_oj/test.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-int main(){
-    char s[20];
-    int a, b, c;
-    scanf("%s %d %d %d", s, &a ,&b ,&c);
-    printf("%s %d %d %d",s,a,b,c);
+#include <stdio.h>
+
+int main(void) {
+    char a, b;
+    scanf("%c %c", &a, &b);
+    printf("%c %c", a, b);
+    return 0;
 }
