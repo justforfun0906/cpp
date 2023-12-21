ef5039f859b4121702ec9128a28e660f8ef8337b
justforfun0906 <jimmy0906abc@gmail.com>
2023-12-21 15:59:50 +0800

    flip flop

--- nthu_oj/13402.c
+++ nthu_oj/13402.c
@@ -1,63 +1,65 @@
 #include<stdio.h>
-int board[16][16];
-int check(int n, int m){
-    int flag = 1;
-    //printf("now:\n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i-1>=0){
-                if(board[i][j]!=board[i-1][j]) flag = 0;
-            }
-            if(j<m-1){
-                if(board[i][j]!=board[i][j+1]) flag = 0;
-            }
-            if(flag != 1)break;
-           // printf("%d",board[i][j]);
-        }
-        //printf("\n");
-    }
-    return flag;
-}
-void mod(int n,int m,int i, int j){
-    board[i][j]=(board[i][j]+1)%2;
-    if(i-1>=0) board[i-1][j]=(board[i-1][j]+1)%2;
-    if(i+1<n) board[i+1][j]=(board[i+1][j]+1)%2;
-    if(j-1>=0) board[i][j-1]=(board[i][j-1]+1)%2;
-    if(j+1<m) board[i][j+1]=(board[i][j+1]+1)%2;
-}
-int flip(int n,int m, int last_i, int last_j,int cnt){//
-    int result = -1;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i!=last_i&&j!=last_j){
-                mod(n,m,i,j);
-                if(check(n,m)){
-                    result = cnt;
-                    return result;
-                }else{
-                    result = flip(n,m,i,j,cnt+1);
-                }
-                mod(n,m,i,j);
+#include<stdlib.h>
+#include<string.h>
+int graph[20][20],N,M;//1_base
+int check(){
+    int stat = graph[1][1];
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=M;j++){
+            if(graph[i][j]!=stat){
+                return 0;
             }
         }
     }
-    return result;
+    return 1;
+}
+void flip(int id){
+    int x = id/M + 1;
+    int y = id%M + 1;
+    graph[x][y]^=1;
+    graph[x+1][y]^=1;
+    graph[x-1][y]^=1;
+    graph[x][y+1]^=1;
+    graph[x][y-1]^=1;
+}
+void flip_all(int st){
+    for(int id=0;id<(N*M);id++){
+        if(st&(1<<id)) flip(id);
+    }
+}
+int one_cnt(int st){
+    int cnt = 0;
+    for(int i=0;i<N*M;i++){
+        if(st&(1<<(N*M)))cnt++;
+    }
+    return cnt;
 }
 int main(){
-    int n, m;
-    scanf("%d %d\n",&n,&m);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            char temp;
-            scanf(" %c",&temp);
-            if(temp=='b') board[i][j]=0;
-            else board[i][j]=1;
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&N,&M);
+        for(int i=1;i<=N;i++){
+            for(int j = 1; j <= M ;j++){
+                char temp;
+                scanf(" %c", &temp);
+                if(temp == 'b') graph[i][j] = 1;
+                else graph[i][j] = 0;
+            }
         }
-    }
-    int result = flip(n,m,-1,-1,0);
-    if(result != -1){
-        printf("%d\n", result);
-    }else{
-        printf("opps\n");
+        int MIN = 7122;
+        for(int st = 0 ; st<(1<<(N*M));st++){
+            int ocnt = one_cnt(st);
+            if(ocnt>MIN) continue;
+            else{
+                flip_all(st);
+                if(check()){
+                    if(ocnt<MIN)MIN = ocnt;
+                }
+                flip_all(st);
+            }
+        }
+        if(MIN!=7122)printf("%d\n",MIN);
+        else printf("oops\n");
     }
 }
