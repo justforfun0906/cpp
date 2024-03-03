6fcc5cdb436adf2eded96ef22fbffc8e2a284b17
justforfun0906 <jimmy0906abc@gmail.com>
2024-03-03 22:29:35 +0800

    play game ac

--- nthu_oj/14218.c
+++ nthu_oj/14218.c
@@ -5,6 +5,18 @@ void solve(){
     for(int i=1;i<=n;i++){
         next[i] = i+1;
     }
+    next[n] = -1;
+    for(int i=0;i<k;i++){
+        int num; scanf("%d", &num);
+        if(next[num] == -1){
+            printf("Penguin07 QQ\n");
+        }else{
+            printf("%d\n", next[num]);
+            int temp_ind = next[num];
+            next[num] = next[next[num]];
+            next[temp_ind] = -1;
+        }        
+    }    
 }
 int main(){
     int t;
