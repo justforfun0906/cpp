bb3d0c26af1572ef8309cca8bffc6973ddb7eafc
justforfun0906 <jimmy0906abc@gmail.com>
2024-03-03 19:42:23 +0800

    bobo

---
+++ nthu_oj/14218.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void solve(){
+    int n,k; scanf("%d%d", &n, &k);
+    int next[n+5];
+    for(int i=1;i<=n;i++){
+        next[i] = i+1;
+    }
+}
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        solve();
+    }
+}
