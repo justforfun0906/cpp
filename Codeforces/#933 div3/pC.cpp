4b29c0b50d92448696c28a972625debcae31ac78
justforfun0906 <jimmy0906abc@gmail.com>
2024-03-12 10:46:18 +0800

    ==

--- Codeforces/#933 div3/pC.cpp
+++ Codeforces/#933 div3/pC.cpp
@@ -5,48 +5,17 @@ using namespace std;
 void solve(){
     int n; cin>>n;
     string s; cin>>s;
-    vector<int> p_front_cnt(n+5,0), e_cnt(n+5,0), m_cnt(n+5,0), p_back_cnt(n+5,0);
-    if(s[0]=='p') p_front_cnt[0] = 1;
-    else if(s[0]=='m') m_cnt[0] = 1;
-    for(int i=1;i<n;i++){
-        if(s[i]=='p') p_front_cnt[i] = p_front_cnt[i-1]+1;
-        else if(s[i]=='m') m_cnt[i] = m_cnt[i-1]+1;
-        else{
-            p_front_cnt[i] = 0;
-            m_cnt[i] = 0;
-        }
-    }
-    for(int i = n-1;i>=0;i--){
-        if(s[i]=='e') e_cnt[i] = e_cnt[i+1]+1;
-        else if(s[i]=='p') p_back_cnt[i] = p_back_cnt[i+1]+1;
-        else{
-            e_cnt[i] = 0;
-            p_back_cnt[i] = 0;
-        }
-    }
-    int ans = 0;
+    int cnt = 0;
     for(int i=0;i<n;i++){
         if(s[i]=='a'){
-            if(p_back_cnt[i+1]>0){
-                if(m_cnt[i-1]>0){
-                    int p_cnt = p_back_cnt[i+1];
-                    if(s[i+p_cnt+1]=='i'&&s[i+p_cnt+2]=='e'){
-                        if(p_back_cnt[i+1]<m_cnt[i-1]+e_cnt[i+2+p_cnt]){
-                            ans+=p_back_cnt[i+1];
-                            p_front_cnt[i+p_cnt] = 0;
-                            i+=p_back_cnt[i+1]+1;
-                            continue;
-                            //cout<<"special\n";
-                        }
-                    }
-                }
-            }
-            ans+=min(p_back_cnt[i+1],m_cnt[i-1]);
+            if(s[i-1]=='m'&&s[i+1]=='p')cnt++;
         }else if(s[i]=='i'){
-            ans+=min(p_front_cnt[i-1],e_cnt[i+1]);
+            if(s[i-1]=='p'&&s[i+1]=='e')cnt++;
+        }else if(s[i]=='p'){
+            if(s[i-1]=='a'&&s[i+1]=='i'&&s[i-2]=='m'&&s[i+2]=='e')cnt--;
         }
     }
-    cout<<ans<<'\n';
+    cout<<cnt<<'\n';
 }
 int main(){
     int t; cin>>t;
