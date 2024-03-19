4d7f606fda8fa4bb0a04c8f954f478509521e0ed
justforfun0906 <jimmy0906abc@gmail.com>
2024-03-20 00:13:14 +0800

    wtf

--- nthu_oj/11833.c
+++ nthu_oj/11833.c
@@ -17,15 +17,14 @@ void print_post_order(node* root){
     if(root->left != NULL) print_post_order(root->left);
     if(root->right != NULL) print_post_order(root->right);
     printf("%d ", root->data);
-    return;
 }
 node* build_tree(int *inorder, int *preorder, int inorder_start, int inorder_end, int preorder_start){
-    for(int i=preorder_start;i<=preorder_start+inorder_end-inorder_start;i++){//interate by the size of the subtree
+    for(int i=preorder_start;i<preorder_start+inorder_end-inorder_start+1;i++){
         for(int j=inorder_start;j<=inorder_end;j++){
-            if(preorder[i] == inorder[j]){//find the root
+            if(preorder[i] == inorder[j]){
                 node* root = create_node(preorder[i]);
                 root->left = build_tree(inorder, preorder, inorder_start, j-1, preorder_start+1);
-                root->right = build_tree(inorder, preorder, j+1, inorder_end, preorder_start+(j-inorder_start)+1);//j-inorder_start is the size of the left subtree
+                root->right = build_tree(inorder, preorder, j+1, inorder_end, preorder_start+j-inorder_start+1);
                 return root;
             }
         }
