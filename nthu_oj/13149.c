ac91808c1a6e6a4da36c84174463503f81a8165c
justforfun0906 <jimmy0906abc@gmail.com>
2024-03-26 14:54:14 +0800

    easy peasy

--- nthu_oj/13149.c
+++ nthu_oj/13149.c
@@ -1,32 +1,64 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
+char value[200005];
+char expr[200005];
 typedef struct _node{
-    char data;
-    int value;
+    int data;//to store the num only, doesn't need to store the operator
     struct _node* left, *right;
 }node;
 int N = 0;
 int idx = 0;
-node* build_tree(){
-
+node* new_node(int data){
+    node* temp = (node*)malloc(sizeof(node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+node* EXPR(){
+    int num = 0;
+    while(isdigit(expr[idx])&&idx<N){//parse the number
+        num = num*10 + (expr[idx]-'0');
+        idx++;
+    }
+    node* root = new_node(num);
+    if(expr[idx]=='?'){//if it's a conditional operator
+        idx++;//skip '?'
+        root->left = EXPR();
+        root->right = EXPR();
+        return root;//return the root of the subtree
+    }
+    idx++;//skip ':'
+    return root;
 }
 int eval(node* root){
-    
+    if(root->left==NULL&&root->right==NULL){
+        return value[root->data-1]-'0';
+    }else{
+        if(value[root->data-1]=='1') return eval(root->left);
+        else{
+            return eval(root->right);
+        }
+    }
+}
+void print_preorder(node* root){
+    if(root==NULL) return;
+    printf("%c",root->data);
+    print_preorder(root->left);
+    print_preorder(root->right);
+    return;
 }
 char expr[200005];
 int main(){
     scanf("%s",expr);
     N = strlen(expr);
-    node* root = build_tree();
+    idx = 0;
     int q; scanf("%d",&q);
+    node* root = EXPR();
+    //print_preorder(root);
     while(q--){
-        int value[N+5];
-        char temp[200005];
-        scanf("%s",temp);
-        for(int i=0;i<N;i++){
-            value[i+1] = temp[i]-'0';
-        }
+        scanf("%s",value);
         printf("%d\n",eval(root));
     }
 }
--- nthu_oj/tempCodeRunnerFile.c
+++ nthu_oj/tempCodeRunnerFile.c
@@ -1,41 +1,71 @@
 #include<stdio.h>
 #include<stdlib.h>
-int N;
-int direct_seafu[1010];
-int find_distance(int a){//actually can be optimized by using a array to store the distance
-    int distance = 0;
-    if(direct_seafu[a]!=1){
-        distance = find_distance(direct_seafu[a])+1;
-    }
-    else{
-        distance = 1;
-    }
-    return distance;
+#include<string.h>
+int value[200005];
+char expr[200005];
+typedef struct _node{
+    char data;
+    int value;
+    struct _node* left, *right;
+}node;
+int N = 0;
+int idx = 0;
+node* new_node(char data){
+    node* temp = (node*)malloc(sizeof(node));
+    temp->data = data;
+    temp->value = -1;//not evaluated yet
+    temp->left = temp->right = NULL;
+    return temp;
 }
-int query(int a, int remain){
-    if(remain!=0&&direct_seafu[a]==a){
-        return -1;
-    }else{
-        if(remain!=0){
-            return query(direct_seafu[a], remain-1);
+node* EXPR(){
+    if(idx>=N-1||expr[idx+1]!='?'){
+        node* root = new_node(expr[idx]);
+        printf("making a leaf node with data %c and idx +=2\n",expr[idx-2]);
+        //return root;
+    }
+    if(expr[idx+1]=='?'){
+        node* root = new_node(expr[idx]);
+        idx+=2;
+        //printf("making an expr root with data %c\n",expr[idx]);
+        root->left=EXPR();
+        idx+=2;
+        //printf("left node with data %c\n",root->left->data);
+        root->right=EXPR();
+        //printf("right node with data %c\n",root->right->data);
+        return root;
+    }
+}
+int eval(node* root){
+    if(root->left!=NULL&&root->right!=NULL){
+        if(value[(root->data)-'0']==1){
+            return eval(root->left);
         }else{
-            return a;
+            return eval(root->right);
         }
+    }else{
+        return value[root->data-'0'];
     }
 }
+void print_preorder(node* root){
+    if(root==NULL) return;
+    printf("%c",root->data);
+    print_preorder(root->left);
+    print_preorder(root->right);
+}
+char expr[200005];
 int main(){
-    scanf("%ds", &N);
-    direct_seafu[1] = 1;//master LJH Orz
-    for(int i=2;i<=N;i++){
-        scanf("%d", &direct_seafu[i]);
-    }
-    for(int i=2;i<=N;i++){
-        printf("%d ", find_distance(i));
-    }
-    int q; scanf("%d", &q);
+    scanf("%s",expr);
+    N = strlen(expr);
+    int q; scanf("%d",&q);
+    node* root = EXPR();
+    //print_preorder(root);
     while(q--){
-        int a, b;
-        scanf("%d %d", &a, &b);
-        printf("%d\n", query(a, b));
+        char temp[200005];
+        scanf("%s",temp);
+        idx = 0;
+        for(int i=0;i<N;i++){
+            value[i+1] = temp[i]-'0';
+        }
+        printf("%d\n",eval(root));
     }
 }
