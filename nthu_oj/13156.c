95782a69fe51148c21516fab85849237fbfb96ce
justforfun0906 <jimmy0906abc@gmail.com>
2024-03-29 13:14:11 +0800

    pabo?

--- nthu_oj/13156.c
+++ nthu_oj/13156.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 int N;
-int in_order_pos[200010];
+int pos[200010];
 int child[200010][2] = {0};
 int parent[200010] = {0};
 int ans = 1;
@@ -9,30 +9,30 @@ void sol(int root, int l, int r){
     if(!ans) return;
     if(child[root][0] && child[root][1]){//has 2 children
     //they gotta be 1 left and 1 right compared to the root
-        if((in_order_pos[child[root][0]]> in_order_pos[root] && in_order_pos[child[root][0]]<=r)//child 0 is right and it's in the range
-        && (in_order_pos[child[root][1]]<in_order_pos[root] && in_order_pos[child[root][1]]>=l)){//child 1 is left and it's in the range
-            sol(child[root][0],in_order_pos[root]+1,r);//check right child
-            sol(child[root][1],l,in_order_pos[root]-1);//check left child
+        if((pos[child[root][0]]> pos[root] && pos[child[root][0]]<=r)//child 0 is right and it's in the range
+        && (pos[child[root][1]]<pos[root] && pos[child[root][1]]>=l)){//child 1 is left and it's in the range
+            sol(child[root][0],pos[root]+1,r);//check right child
+            sol(child[root][1],l,pos[root]-1);//check left child
         }
-        else if((in_order_pos[child[root][1]]> in_order_pos[root] && in_order_pos[child[root][1]]<=r)//child 1 is right and it's in the range
-        && (in_order_pos[child[root][0]]<in_order_pos[root] && in_order_pos[child[root][0]]>=l)){//child 0 is left and it's in the range
-            sol(child[root][1],in_order_pos[root]+1,r);//check right child
-            sol(child[root][0],l,in_order_pos[root]-1);//check left child
+        else if((pos[child[root][1]]> pos[root] && pos[child[root][1]]<=r)//child 1 is right and it's in the range
+        && (pos[child[root][0]]<pos[root] && pos[child[root][0]]>=l)){//child 0 is left and it's in the range
+            sol(child[root][1],pos[root]+1,r);//check right child
+            sol(child[root][0],l,pos[root]-1);//check left child
         }
-        else ans = 0;
+        else ans = 0;//if the children are not in the right position
     }else if(child[root][0]){//only has 1 child
-        if(in_order_pos[child[root][0]]<in_order_pos[root] && in_order_pos[child[root][0]]>=l){//child is left
-            sol(child[root][0],l,in_order_pos[root]-1);
-        }else if(in_order_pos[child[root][0]]>in_order_pos[root] && in_order_pos[child[root][0]]<=r){//child is right
-            sol(child[root][0],in_order_pos[root]+1,r);
+        if(pos[child[root][0]]<pos[root] && pos[child[root][0]]>=l){//child is left
+            sol(child[root][0],l,pos[root]-1);
+        }else if(pos[child[root][0]]>pos[root] && pos[child[root][0]]<=r){//child is right
+            sol(child[root][0],pos[root]+1,r);
         }else{
-            ans = 0;
+            ans = 0;//if the child is not in the right position
         }
     }else if(child[root][1]){//only has 1 child
-        if(in_order_pos[child[root][1]]<in_order_pos[root] && in_order_pos[child[root][1]]>=l){//child is left
-            sol(child[root][1],l,in_order_pos[root]-1);
-        }else if(in_order_pos[child[root][1]]>in_order_pos[root] && in_order_pos[child[root][1]]<=r){//child is right
-            sol(child[root][1],in_order_pos[root]+1,r);
+        if(pos[child[root][1]]<pos[root] && pos[child[root][1]]>=l){//child is left
+            sol(child[root][1],l,pos[root]-1);
+        }else if(pos[child[root][1]]>pos[root] && pos[child[root][1]]<=r){//child is right
+            sol(child[root][1],pos[root]+1,r);
         }else{
             ans = 0;
         }
@@ -44,7 +44,7 @@ void solve(){
     ans = 1;
     for(int i=0;i<=N;i++){
         parent[i] = 0;
-        in_order_pos[i] = 0;
+        pos[i] = 0;
         child[i][0] = 0;
         child[i][1] = 0;
     }
@@ -57,7 +57,7 @@ void solve(){
     for(int i=1;i<=N;i++){
         int temp ;
         scanf("%d",&temp);
-        in_order_pos[temp] = i;
+        pos[temp] = i;
     }
     int root;
     for(int i=1;i<=N;i++){
--- nthu_oj/13826.h
+++ nthu_oj/13826.h
@@ -9,22 +9,22 @@ void eFormSort(Node *head){
     Node *p = head;
     Node *p_pre = NULL;
     int now = 1;
-    while(p != NULL){
-        if(p->serial != now){
+    while(p != NULL){//until reach the end
+        if(p->serial != now){//if the serial number of p is not equal to the value that we are finding
             Node *q = p;
             Node *q_pre = p_pre;
-            while(q->serial != now){
+            while(q->serial != now){//finding target node
                 q_pre = q;
                 q = q->next;
             }
-            q_pre->next = q->next;
+            q_pre->next = q->next;//move q to the front of p
             q->next = p;
             p_pre->next = q;
-            p = q;
+            p = q;//important
             //printf("swap %d %d\n", p->serial, q->serial);
         }
         p_pre = p;
         p = p->next;
-        now++;
+        now++;//move to the next node
     }
 }
