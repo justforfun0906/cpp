d78fe4447b6433f7799d2dc0b47bca5b73440446
justforfun0906 <jimmy0906abc@gmail.com>
2024-04-01 21:04:35 +0800

    lol

--- nthu_oj/13485.cpp
+++ nthu_oj/13485.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
-#include<string>
-#include<vector>
+#include"13485.hpp"
 using namespace std;
 int main(){
-    
+    int n;
+    string x, y;
+    cin >> n;
+    while(n--){
+        cin >> x >> y;
+        String_Calculator a(x), ans;
+        ans = a.Subtract(y).Add(y).DividedBy(y);
+        ans.output();
+    }
 }
---
+++ nthu_oj/13485.hpp
@@ -0,0 +1,21 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+#include <iostream>
+using namespace std;
+
+class String_Calculator {
+    public:
+        String_Calculator(){};
+        String_Calculator(const string);
+        String_Calculator& Add(const string);
+        String_Calculator& Subtract(const string);
+        String_Calculator& DividedBy(const string);
+        void output(){
+            cout << s << endl;
+        }
+
+    private:
+        string s = "";
+}; //end class String_Calculator
+
+#endif // FUNCTION_H
