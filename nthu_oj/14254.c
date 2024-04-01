dbf4a23274a718ad28ba9f8517ada5f39a191d6c
justforfun0906 <jimmy0906abc@gmail.com>
2024-04-01 14:45:59 +0800

    april fool

--- nthu_oj/14254.c
+++ nthu_oj/14254.c
@@ -14,6 +14,11 @@ int idx = 0;
 //TODO: use ans to store the result of the expression tree
 //TODO: only calculate the ans when it is swapped
 // Function to build the expression tree from the input string.
+void reset(Node* t){
+    if(t==NULL) return;
+    t->ans = -1;//ans = -1 means not calculated yet
+    reset(t->p);
+}
 Node *build() {
     if(idx >= n) return NULL;
     Node *r = (Node *)malloc(sizeof(Node));
@@ -38,15 +43,18 @@ Node *build() {
 
 // Function to evaluate the expression tree and calculate its beauty.
 int eval(Node *t) {
-    if(t->val>=0) return t->val;
+    if(t->ans!=-1) return t->ans;
     if(t->val== -1){
-        return ((1ll*eval(t->lc) + eval(t->rc)) % mod+mod)%mod;
+        return t->ans=((1ll*eval(t->lc) + eval(t->rc)) % mod+mod)%mod;
     }
     if(t->val== -2){
-        return ((1ll*eval(t->lc) - eval(t->rc)) % mod+mod)%mod;
+        return t->ans=((1ll*eval(t->lc) - eval(t->rc)) % mod+mod)%mod;
     }
     if(t->val== -3){
-        return ((1ll*eval(t->lc) * eval(t->rc)) % mod+mod)%mod;
+        return t->ans=((1ll*eval(t->lc) * eval(t->rc)) % mod+mod)%mod;
+    }
+    else{
+        return t->ans = t->val;
     }
 }
 // Function to swap the subtrees rooted at nodes a and b.
@@ -63,8 +71,19 @@ void swap(Node *a, Node *b) {
     a->val = b->val;
     b->val = temp_val;
 
-}
+    int temp_ans = a->ans;
+    a->ans = b->ans;
+    b->ans = temp_ans;
 
+    reset(a->p);
+    reset(b->p);
+}
+void print_preorder(Node* root){
+    if(root == NULL) return;
+    printf("%d ", root->ans);
+    print_preorder(root->lc);
+    print_preorder(root->rc);
+}
 // Depth-First Search function to check if target node is in the subtree rooted at 'now'.
 bool dfs(Node *now, Node *target) {
     if(now == NULL) return false;
@@ -81,16 +100,24 @@ int main() {
     scanf("%d %d", &h, &t);
     scanf("%s", expr);
     n = (1 << (h + 1)) - 1; // n = 2 ^ (h + 1) - 1
-    build();
-    int ans = eval(tree[0]);
+    tree[0]= build();
+    int ans = tree[0]->ans;
+    //print_preorder(tree[0]);
+    //printf("\n");
     if(t == 1) {
         for(int i=1;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(!check(tree[i], tree[j])){
                     swap(tree[i], tree[j]);
+                    //printf("now swapping %d %d\n", i, j);
+                    //print_preorder(tree[0]);
+                    //printf("\n");
                     int temp = eval(tree[0]);
                     ans = temp > ans ? temp : ans;
+                    //printf("swapping back %d %d\n", i, j);
                     swap(tree[i], tree[j]);
+                    //print_preorder(tree[0]);
+                    //printf("\n");
                 }
             }
         }
@@ -99,9 +126,15 @@ int main() {
         for(int i=0;i<n;i++){
             if(tree[i]->lc!=NULL&&tree[i]->rc!=NULL){
                 swap(tree[i]->lc, tree[i]->rc);
+                /*printf("now swapping %d\n", i);
+                print_preorder(tree[0]);
+                printf("\n");*/
                 int temp = eval(tree[0]);
                 ans = temp > ans ? temp : ans;
+                //printf("swapping back %d\n", i);
                 swap(tree[i]->lc, tree[i]->rc);
+                /*print_preorder(tree[0]);
+                printf("\n");*/
             }
         }
     }
