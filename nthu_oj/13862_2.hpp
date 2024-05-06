c393e09869f167ef4ccb97b819927616ba100e32
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-06 10:42:11 +0800

    pineapple sensei 3/6 ...

---
+++ nthu_oj/13862_2.hpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+enum Gender {
+    MALE,
+    FEMALE
+};
+
+const string table[2] = {"MALE", "FEMALE"};
+
+class Person {
+    public:
+        string name;
+        int age;
+        Gender gender;
+        string personality;
+        Person *parentA, *parentB, *mate, *child;
+
+        Person() {
+            this->name = "";
+            this->age = 0;
+            this->gender = MALE;
+            this->personality = "";
+            this->parentA = nullptr;
+            this->parentB= nullptr;
+            this->mate = nullptr;
+            this->child = nullptr;
+        }
+        void describe(string* arr, int now, int len);
+        Person* getRelative (string* arr, int now, int len);
+        string info() {
+            return 
+                "Name:" + this->name + ",\n" + 
+                "Age:" + to_string(this->age) + ",\n" +
+                "Gender:" + table[this->gender] + ",\n" +
+                "Personality:" + this->personality + "\n\n";
+        };
+};
+void Person::describe(string* arr, int now, int len){
+    Person* target = getRelative(arr, 0, len-2);
+    if(arr[len-2]=="Name"){
+        target->name = arr[len-1];
+    }else if(arr[len-2]=="Age"){
+        target->age = stoi(arr[len-1]);
+    }else if(arr[len-2]=="Personality"){
+        target->personality += target->personality=="" ? arr[len-1] : " " + arr[len-1];
+    }else if(arr[len-2]=="Gender"){
+        target->gender = arr[len-1]=="MALE"?MALE:FEMALE;
+    }
+}
+//arr is a array of string.
+Person* Person::getRelative(string* arr, int now, int len){
+    if(now==len) return this;
+    if(arr[now]=="ParentA"){
+        if(this->parentA==nullptr){
+            this->parentA = new Person();
+            this->parentA->child = this;
+            if(this->parentB!=nullptr){//not likely to happen
+                this->parentA->mate = this->parentB;
+                this->parentB->mate = this->parentA;
+            }
+        }
+        return this->parentA->getRelative(arr, now+1, len);
+    }else if(arr[now]=="ParentB"){
+        if(this->parentB==nullptr){
+            this->parentB = new Person();
+            this->parentB->child = this;
+            if(this->parentA!=nullptr){
+                this->parentB->mate = this->parentA;
+                this->parentA->mate = this->parentB;
+            }
+        }
+        return this->parentB->getRelative(arr, now+1, len);
+    }else if(arr[now]=="Mate"){
+        if(this->mate==nullptr){
+            this->mate = new Person();
+            this->mate->mate = this;
+        }
+        if(this->child!=nullptr){
+            this->mate->child = this->child;
+            this->child->parentA = this;
+            this->child->parentB = this->mate;
+        }
+        return this->mate->getRelative(arr, now+1, len);
+    }else if(arr[now]=="Child"){
+        if(this->child == nullptr){
+            this->child = new Person();
+            this->child->parentA = this;
+        }
+        if(this->mate!=nullptr){
+            this->child->parentB = this->mate;
+            this->mate->child = this->child;
+        }
+        return this->child->getRelative(arr, now+1, len);
+    }
+}
