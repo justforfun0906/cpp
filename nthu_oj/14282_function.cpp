0d6da6ea7930099a1655a135a73680bc338b7073
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-09 00:07:50 +0800

    sui la

--- nthu_oj/14282_function.cpp
+++ nthu_oj/14282_function.cpp
@@ -1,6 +1,6 @@
-#include "14282.hpp"
+#include "function.h"
 #include <iostream>
-Board::Board(int r,int c):rows(r),cols(c),gameTurn(0){
+Board::Board(int r, int c): rows(r), cols(c), gameTurn(0){
     board = new char*[rows];
     for(int i=0;i<rows;i++){
         board[i] = new char[cols];
@@ -9,7 +9,7 @@ Board::Board(int r,int c):rows(r),cols(c),gameTurn(0){
         }
     }
 }
-Board::Board(const Board& b):rows(b.rows),cols(b.cols),gameTurn(b.gameTurn){
+Board::Board(const Board& b): rows(b.rows), cols(b.cols), gameTurn(b.gameTurn){
     board = new char*[rows];
     for(int i=0;i<rows;i++){
         board[i] = new char[cols];
@@ -27,93 +27,81 @@ Board::~Board(){
     }
     delete[] board;
 }
-ostream& operator<<(ostream& output,const Board& b){
+ostream& operator<<(ostream& output, const Board& b){
     for(int i=0;i<b.rows;i++){
         for(int j=0;j<b.cols;j++){
             output << b.board[i][j];
         }
-        if(i+1<b.rows) output << endl;
+        if(i+1<b.rows)output << endl;
     }
     return output;
 }
-TicTacToeBoard::TicTacToeBoard():Board(TIC_TAC_TOW_ROWS,TIC_TAC_TOW_COLS){}
+//derived class constructors
+TicTacToeBoard::TicTacToeBoard(): Board(TIC_TAC_TOW_ROWS, TIC_TAC_TOW_COLS){}
+ConnectFourBoard::ConnectFourBoard(): Board(CONNECT_FOUR_ROWS, CONNECT_FOUR_COLS){}
 TicTacToeBoard& TicTacToeBoard::operator+=(const Move& m){
-    (*this)[m.move[0]-'0'][m.move[2]-'0'] = (gameTurn ? 'X' : 'O');
-    gameTurn = nextPlayer();
+    int row = m.move[0]-'0';
+    int col = m.move[2]-'0';
+    (*this)[row][col] = gameTurn?'X':'O';
+    gameTurn= nextPlayer();
     return *this;
 }
 bool TicTacToeBoard::isGameWon(){
-    for(int row=0;row<3;row++){
-        if((*this)[row][0]!='.'&&(*this)[row][0]==(*this)[row][1]&&(*this)[row][1]==(*this)[row][2]){
-            return true;
-        }
+    //check rows and columns
+    for(int i=0;i<TIC_TAC_TOW_ROWS;i++){
+        if((*this)[i][0]!='.' &&
+        (*this)[i][0]==(*this)[i][1] &&
+        (*this)[i][1]==(*this)[i][2])return true;
     }
-    for(int col=0;col<3;col++){
-        if((*this)[0][col]!='.'&&(*this)[0][col]==(*this)[1][col]&&(*this)[1][col]==(*this)[2][col]){
-            return true;
-        }
+    for(int i=0;i<TIC_TAC_TOW_COLS;i++){
+        if((*this)[0][i]!='.' &&
+        (*this)[0][i]==(*this)[1][i] &&
+        (*this)[1][i]==(*this)[2][i])return true;
     }
-    if((*this)[0][0]!='.'&&(*this)[0][0]==(*this)[1][1]&&(*this)[1][1]==(*this)[2][2]){
-        return true;
-    }
-    if((*this)[0][2]!='.'&&(*this)[0][2]==(*this)[1][1]&&(*this)[1][1]==(*this)[2][0]){
-        return true;
-    }
+    //check diagonals
+    if((*this)[0][0]!='.' && 
+        (*this)[0][0]==(*this)[1][1] && 
+        (*this)[1][1]==(*this)[2][2]) return true;
+
+    if((*this)[0][2]!='.' && 
+        (*this)[0][2]==(*this)[1][1] && 
+        (*this)[1][1]==(*this)[2][0]) return true;
     return false;
 }
-ConnectFourBoard::ConnectFourBoard():Board(CONNECT_FOUR_ROWS,CONNECT_FOUR_COLS){}
 ConnectFourBoard& ConnectFourBoard::operator+=(const Move& m){
-    int col = m.move[1]- '0';
-    for(int i=5;i>=0;i--){
+    int col = m.move[1]-'0';
+    for(int i = CONNECT_FOUR_ROWS-1;i>=0;i--){
         if((*this)[i][col]=='.'){
-            (*this)[i][col] = (gameTurn ? 'X' : 'O');
-            gameTurn = nextPlayer();
+            (*this)[i][col] = gameTurn?'B':'W';
             break;
         }
     }
     gameTurn = nextPlayer();
     return *this;
 }
-bool ConnectFourBoard::isGameWon(){
-    for (int row = 0; row < 6; ++row) {
-        for (int col = 0; col < 4; ++col) {
-            if ((*this)[row][col] != '.' &&
-                (*this)[row][col] == (*this)[row][col+1] &&
-                (*this)[row][col+1] == (*this)[row][col+2] &&
-                (*this)[row][col+2] == (*this)[row][col+3]) {
-                return true;
-            }
+bool ConnectFourBoard:: isGameWon(){
+    //check rows
+    for(int i=0;i<CONNECT_FOUR_ROWS;i++){
+        for(int j=0;j<CONNECT_FOUR_COLS-3;j++){
+            if((*this)[i][j]!='.' && (*this)[i][j]==(*this)[i][j+1] && (*this)[i][j+1]==(*this)[i][j+2] && (*this)[i][j+2]==(*this)[i][j+3])return true;
         }
     }
-    for (int col = 0; col < 7; ++col) {
-        for (int row = 0; row < 3; ++row) {
-            if ((*this)[row][col] != '.' &&
-                (*this)[row][col] == (*this)[row+1][col] &&
-                (*this)[row+1][col] == (*this)[row+2][col] &&
-                (*this)[row+2][col] == (*this)[row+3][col]) {
-                return true;
-            }
+    //check columns
+    for(int i=0;i<CONNECT_FOUR_ROWS-3;i++){
+        for(int j=0;j<CONNECT_FOUR_COLS;j++){
+            if((*this)[i][j]!='.' && (*this)[i][j]==(*this)[i+1][j] && (*this)[i+1][j]==(*this)[i+2][j] && (*this)[i+2][j]==(*this)[i+3][j])return true;
         }
     }
-    for (int row = 0; row < 3; ++row) {
-        for (int col = 0; col < 4; ++col) {
-            if ((*this)[row][col] != '.' &&
-                (*this)[row][col] == (*this)[row+1][col+1] &&
-                (*this)[row+1][col+1] == (*this)[row+2][col+2] &&
-                (*this)[row+2][col+2] == (*this)[row+3][col+3]) {
-                return true;
-            }
+    //check diagonals
+    for(int i=0;i<CONNECT_FOUR_ROWS-3;i++){
+        for(int j=0;j<CONNECT_FOUR_COLS-3;j++){
+            if((*this)[i][j]!='.' && (*this)[i][j]==(*this)[i+1][j+1] && (*this)[i+1][j+1]==(*this)[i+2][j+2] && (*this)[i+2][j+2]==(*this)[i+3][j+3])return true;
         }
     }
-    for (int row = 3; row < 6; ++row) {
-        for (int col = 0; col < 4; ++col) {
-            if ((*this)[row][col] != '.' &&
-                (*this)[row][col] == (*this)[row-1][col+1] &&
-                (*this)[row-1][col+1] == (*this)[row-2][col+2] &&
-                (*this)[row-2][col+2] == (*this)[row-3][col+3]) {
-                return true;
-            }
+    for(int i=0;i<CONNECT_FOUR_ROWS-3;i++){
+        for(int j=3;j<CONNECT_FOUR_COLS;j++){
+            if((*this)[i][j]!='.' && (*this)[i][j]==(*this)[i+1][j-1] && (*this)[i+1][j-1]==(*this)[i+2][j-2] && (*this)[i+2][j-2]==(*this)[i+3][j-3])return true;
         }
     }
-    return false;    
+    return false;
 }
