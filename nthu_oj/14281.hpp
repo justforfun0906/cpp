97b83b296f5df76dd078a0d6e10b493df7e7caae
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-08 15:52:17 +0800

    test cases are not strong enough(cnt wont exceed 9)

--- nthu_oj/14281.hpp
+++ nthu_oj/14281.hpp
@@ -2,11 +2,13 @@
 #define _FUNC_H
 #include <iostream>
 #include <string>
+#include <cctype>
+#include <sstream>  
 using namespace std;
 
 class Codec {
 public:
-    Codec(string s): encoded{false}, code_str{s} { }
+    Codec(string s): encoded(false), code_str(s) { }
     virtual ~Codec() { } // virtual destructor; do nothing
     virtual void encode() = 0;
     virtual void decode() = 0;
@@ -19,11 +21,80 @@ protected:
     string code_str;
 };
 class DummyCodec: public Codec {
-
+public:
+    DummyCodec(string s): Codec(s) { }
+    void encode() override {
+        encoded = true;
+    }
+    void decode() override {
+        encoded = false;
+    }
 };
 class RleCodec: public Codec {
-
+public:
+    RleCodec(string s): Codec(s) { }
+    void encode() override {
+        stringstream ss;
+        int cnt = 0;
+        char prev = 0;
+        for(auto c: code_str){
+            if(c!=prev){
+                if(cnt>0){
+                    if(cnt>2){
+                        ss << cnt << prev;
+                    } else if(cnt==2){
+                        ss << prev << prev;
+                    } else {
+                        ss << prev;
+                    }
+                }
+                prev = c;
+                cnt = 1;
+            }else{
+                cnt++;
+            }
+        }
+        if(cnt>0){
+            if(cnt>2){
+                ss << cnt << prev;
+            } else if(cnt==2){
+                ss << prev << prev;
+            } else {
+                ss << prev;
+            }
+        }
+        encoded = true;
+        code_str = ss.str();
+    }
+    void decode() override {
+        string res;
+        string cnt = "";
+        for(int i=0;i<code_str.size();i++){
+            if(isdigit(code_str[i])){
+                cnt += code_str[i];
+            }else{
+                if(!cnt.empty()){
+                    int num = stoi(cnt);
+                    for(int j=0;j<num;j++){
+                        res += code_str[i];
+                    }
+                    cnt.clear();
+                }else{
+                    res += code_str[i];
+                }
+            }
+        }
+        code_str = res;
+        encoded = false;
+    }
 };
-Codec* getCodec(const string& type, const string& is);
+Codec* getCodec(const string& type, const string& is){
+    if (type == "Dummy") {
+        return new DummyCodec(is);
+    } else if (type == "RLE") {
+        return new RleCodec(is);
+    }
+    return nullptr;
+}
 
 #endif
