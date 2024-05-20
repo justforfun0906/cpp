97ddf1ae3a20e48f123882cb46723cd945346abf
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-20 13:49:02 +0800

    yeah lower bound

---
+++ nthu_oj/14321.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int N,M; cin>>N>>M;
+    set<int> A;
+    for(int i=0;i<N;i++){
+        int x; cin>>x;
+        A.insert(x);
+    }
+    for(int i=0;i<M;i++){
+        int x; cin>>x;
+        auto it = A.lower_bound(x);
+        if(*it==x){
+            cout<<*it<<'\n';
+        } else {
+            set<int>::iterator prev;
+            if(it!=A.begin()){
+                prev = it;
+                prev--;
+            } else {
+                cout<<*it<<'\n';
+                continue;
+            }
+            int front_dis = abs(*prev-x);
+            int back_dis = abs(*it-x);
+            if(front_dis<=back_dis){
+                cout<<*prev<<'\n';
+            } else {
+                cout<<*it<<'\n';
+            }
+        }
+    }
+}
