0eb9689c19400e88d548f94072685deb87790ff5
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-27 20:29:24 +0800

    RE 2/4

---
+++ nthu_oj/12306.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef struct player_state{
+    int hp;
+    int mhp;
+    int lv;
+    int step;
+    player_state(int hp,int mhp,int lv,int step):hp(hp),mhp(mhp),lv(lv),step(step){}
+} state;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int L, HP, MHP, MDMG;
+    cin>>L>>HP>>MHP>>MDMG;
+    vector<pair<int, int> > lv_table(L+5);
+    for(int i=0;i<L;i++){
+        cin>>lv_table[i].first>>lv_table[i].second;
+        //first = dmg, second = hl(amount of hp that can be healed)
+    }
+    queue<state> q;
+    q.push(state(HP, MHP, 0, 0));
+    int min_step = 1e9;
+    while(!q.empty()){
+        state cur = q.front();
+        q.pop();
+        //attack
+        if(cur.mhp-lv_table[cur.lv].first<=0){
+            //cout<<"win step = "<<cur.step+1<<endl;
+            min_step = min(min_step, cur.step+1);
+        }else if(cur.hp-MDMG>0){
+            //cout<<"attack "<<cur.hp-MDMG<<" "<<cur.mhp-lv_table[cur.lv].first<<" "<<cur.lv<<" "<<cur.step+1<<endl;
+            if(cur.step+1<min_step)q.push(state(cur.hp-MDMG, cur.mhp-lv_table[cur.lv].first, cur.lv, cur.step+1));
+        }
+        //heal
+        int temp_hp = min(cur.hp+lv_table[cur.lv].second, HP);
+        temp_hp -= MDMG;
+        if(temp_hp>0){
+            //cout<<"heal "<<temp_hp<<" "<<cur.mhp<<" "<<cur.lv<<" "<<cur.step+1<<endl;
+            if(cur.step+1<min_step)q.push(state(temp_hp, cur.mhp, cur.lv, cur.step+1));
+        }
+        //level up
+        if(cur.lv+1<L&&cur.hp-MDMG>0){
+            //cout<<"level up "<<cur.hp-MDMG<<" "<<cur.mhp<<" "<<cur.lv+1<<" "<<cur.step+1<<endl;
+            if(cur.step+1<min_step)q.push(state(cur.hp-MDMG, cur.mhp, cur.lv+1, cur.step+1));
+        }
+    }
+    cout<<min_step<<endl;
+}
