893a67534615b19f5420cbc0183e8ed972a36dfe
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-31 18:33:22 +0800

    cry baby

--- nthu_oj/14300.cpp
+++ nthu_oj/14300.cpp
@@ -1,10 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int M,N;
+void print(vector<int> &v){
+    for(auto i:v){
+        cout<<i<<'\n';
+    }
+}
+int find_job(map<int, queue<int>> &ma, int target){
+    if(ma.count(target)){//has a queue for target job type
+        int ret_val = ma[target].front();
+        ma[target].pop();
+        if(ma[target].empty()){//take the last one
+            ma.erase(target);
+        }
+        return ret_val;
+    }else{
+        return -1;//not found
+    }
+}
 int main(){
     cin>>M>>N;
     vector<int> job_set[M+5];
-    for(int i=1;i<=M;i++){
+    for(int i=0;i<M;i++){
         int k; cin>>k;
         job_set[i].resize(k);
         for(int j=0;j<k;j++){
@@ -16,14 +33,29 @@ int main(){
         int k; cin>>k;
         job_type[k].push(i);
     }
-    vector<int> winner_of_jobs(N+5);
-    for(int i=1;i<=M;i++){
+    vector<int> winner_of_jobs(N);
+    for(int i=0;i<M;i++){
+        vector<int> job_taken_list;
         for(int x:job_set[i]){
-            winner_of_jobs[job_type[x].front()] = i;
-            job_type[x].pop();
+            int ind = find_job(job_type, x);
+            if(ind==-1){//cry
+                print(winner_of_jobs);
+                /*
+                without using job_taken_list
+                a littlecute who can't get all jobs that it want
+                might eventually getting a portion of it's job set
+                however, it should get non of them.
+                by adding a job_taken_list, we can ensure that only those little cutes
+                who don't cry, can get the jobs in their job set.
+                */
+                return 0;
+            }else{
+                job_taken_list.push_back(ind);
+            }
+        }
+        for(auto x:job_taken_list){
+            winner_of_jobs[x] = i+1;
         }
     }
-    for(int i=0;i<N;i++){
-        cout<<winner_of_jobs[i]<<'\n';
-    }
+    print(winner_of_jobs);
 }
