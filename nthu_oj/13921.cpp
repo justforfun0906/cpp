cfb2994e8d5306ede55bca9863ebf69848164a28
justforfun0906 <jimmy0906abc@gmail.com>
2024-05-31 15:34:53 +0800

    remove it instead==

--- nthu_oj/13921.cpp
+++ nthu_oj/13921.cpp
@@ -1,30 +1,27 @@
-/*
-There’s a numberline. Initially, no point is lying on it.
-Given N queries, each is one of the following two types:
-1 x: Add a point x on the numberline. If x is already lying on the numberline, remove it instead.
-
-2 x: There are a lot of segments, separated by the points on the numberline. 
-You have to answer the length of the segments that x is lying on. 
-If x is the endpoints of two segments, x is considered lying on the right one. 
-Furthermore, if the length of the segment is infinite, output −1.
-
-Input:
-7
-2 17
-1 20
-2 4
-1 11
-1 5
-2 10
-1 20
-
-Output:
--1
--1
-6
-*/
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int q;
+    cin>>q;
+    set<int> pts;
+    while(q--){
+        int instruct; cin>>instruct;
+        if(instruct == 1){
+            int x;
+            cin>>x;
+            if(pts.find(x)!=pts.end()) pts.erase(x);
+            else pts.insert(x);
+        }else if(instruct == 2){
+            int x; cin>>x;
+            auto right = pts.upper_bound(x), left = right;
+            if(right != pts.begin())left--;
+            if(right == pts.end()||left == right){
+                cout<<-1<<'\n';
+            }else{
+                cout<<*right - *left<<'\n';
+            }
+        }
+    }
 }
