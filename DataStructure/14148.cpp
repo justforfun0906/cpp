400c694654188cceff92fba6cdfe465c46a4c096
justforfun0906 <jimmy0906abc@gmail.com>
2024-07-07 22:58:54 +0800

    homework done

--- DataStructure/14148.cpp
+++ DataStructure/14148.cpp
@@ -3,32 +3,33 @@ using namespace std;
 int Partition(vector<int> &v, int l, int r){
     int pivot = v[l];
     int i = l+1;
+    //make sure don't change the location of pivot first
     for(int j=l+1;j<=r;j++){
         if(v[j]>pivot){
             swap(v[j],v[i]);
             i++;
         }
     }
+    //from l to i-1, all elements are greater than pivot
+    //swap pivot with i-1
+    //making sure that pivot is in the correct position
     swap(v[l],v[i-1]);
-    // for(int temp = l; temp<=r;temp++){
-    //     cout<<v[temp]<<' ';
-    // }
-    // cout<<'\n';
+    //return the correct position of pivot
     return i-1;
 }
-int QuickSelect(vector<int> v,int l, int r, int target){
+int QuickSelect(vector<int> &v,int l, int r, int target){
     int pos = Partition(v,l,r);
-    if(l==r){
-        return v[l];
-    }
+    //if l == r, pos will = l
     if(pos==target){
         return v[pos];
     }
-    int mid = (l+r)/2;
     if(pos>target){
-        return QuickSelect(v,l,pos,target);
+        return QuickSelect(v,l,pos-1, target);
     }else{
-        return QuickSelect(v,pos+1,r,target-pos);
+        //don't need to change the value of target
+        //it represent the real value of location in the vector
+        //it is not relative
+        return QuickSelect(v,pos+1,r, target);
     }
 }
 int main(){
@@ -36,5 +37,6 @@ int main(){
     cin>>n>>k;
     vector<int> v(n);
     for(int i=0;i<n;i++) cin>>v[i];
-    cout<<QuickSelect(v,0,n-1,k-1);
+    cout<<QuickSelect(v,0,n-1,k-1)<<'\n';
+    return 0;
 }
