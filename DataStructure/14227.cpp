0f477eaf1f0a65152716079121bedd18b893341b
justforfun0906 <jimmy0906abc@gmail.com>
2024-07-10 17:50:15 +0800

    123

--- DataStructure/14227.cpp
+++ DataStructure/14227.cpp
@@ -23,9 +23,7 @@ bool check(string &s){
     return st.empty();
 }
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    //ios_base::sync_with_stdio(0);
     int q;
     cin>>q;
     getchar();
