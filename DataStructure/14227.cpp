47a2ec6a7f5ed4ddf3e3777d216e489d6d91812f
justforfun0906 <jimmy0906abc@gmail.com>
2024-07-10 15:15:55 +0800

    hw

---
+++ DataStructure/14227.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isLeftParen(char c){
+    return c == '(' || c == '[';
+}
+bool check(string &s){
+    int n = s.size();
+    stack<char> st;
+    for(int i=0;i<n;i++){
+        if(isLeftParen(s[i])){
+            st.push(s[i]);
+        }else{
+            if(st.empty())return false;
+            if(s[i] == ')' && st.top() == '('){
+                st.pop();
+            }else if(s[i] == ']' && st.top() == '['){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int q;
+    cin>>q;
+    getchar();
+    while(q--){
+        char c;
+        string s;
+        c = getchar();
+        if(c == '\n' || c == EOF){
+            cout<<"N";
+        }else{
+            while(c != '\n' && c != EOF){
+                s.push_back(c);
+                c = getchar();
+            }
+            check(s)?cout<<"Y":cout<<"N";
+        }
+        if(q!=0)cout<<'\n';
+    }
+}
--- DataStructure/14346.cpp
+++ DataStructure/14346.cpp
@@ -10,5 +10,17 @@ int main(){
         cin>>v[i].first>>v[i].second;
     }
     sort(v.begin(), v.end());
-
+    int ans_min= 1e9;
+    for(int i=0;i<k;i++){
+        pq.push(v[i].second);
+    }
+    ans_min = v[k-1].first + pq.top();
+    for(int i=k;i<n;i++){
+        pq.pop();
+        pq.push(v[i].second);
+        ans_min = min(ans_min, v[i].first + pq.top());
+        // cout<<"x = "<<v[i].first<<" top = "<<pq.top();
+        // cout<<" ans_min = "<<ans_min<<'\n';
+    }
+    cout<<ans_min<<'\n';
 }
